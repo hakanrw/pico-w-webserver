5e90244f1fe0fe727736143c4db0ccee7f1ae9ec
hakan <hakancandar@proton.me>
2024-07-06 17:12:52 +0200

    first push

---
+++ main.c
@@ -0,0 +1,56 @@
+// Copyright (c) 2020-2022 Cesanta Software Limited
+// All rights reserved
+
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+
+#include "lwip/ip4_addr.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "mongoose.h"
+#include "net.h"
+
+
+#define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
+#define TEST_TASK_STACK_SIZE			(( configSTACK_DEPTH_TYPE ) 2048)
+
+static struct mg_mgr mgr;
+
+void main_task(__unused void *params) {
+    if (cyw43_arch_init()) {
+        printf("failed to initialise\n");
+        return;
+    }
+    cyw43_arch_enable_sta_mode();
+    printf("Connecting to WiFi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect.\n");
+        exit(1);
+    } else {
+        printf("Connected.\n");
+    }
+
+    mg_mgr_init(&mgr);
+    web_init(&mgr);
+
+    while(true) {
+        mg_mgr_poll(&mgr, 10);
+    }
+
+    cyw43_arch_deinit();
+}
+
+void vLaunch( void) {
+    TaskHandle_t task;
+    xTaskCreate(main_task, "TestMainThread", TEST_TASK_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &task);
+    vTaskStartScheduler();
+}
+
+int main( void )
+{
+    stdio_init_all();
+    vLaunch();
+    f_mkdir("a.c");
+    return 0;
+}
